2b7ca8e266f186fa8d4f020f2e3328d8135750bc
Song Junwoo <busang.song@gmail.com>
2021-07-04 01:31:59 +0900

    Comment

--- README.md
+++ README.md
@@ -4,5 +4,4 @@ Bmacs=Bolt-Emacs(Bolt is my dog's name)
 
 Bmacs was written by Song Junwoo
 Bmacs (c) Copyright 2021 Song Junwoo
-- busang.song@gmail.com
 - https://github.com/song9063/bmacs
--- screen.c
+++ screen.c
@@ -4,7 +4,6 @@
 
     Bmacs was written by Song Junwoo
     Bmacs (c) Copyright 2021 Song Junwoo
-    - busang.song@gmail.com
     - https://github.com/song9063/bmacs
 
     Bmacs is free software: you can redistribute it and/or modify
